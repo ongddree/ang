0197cc930e03249c59929d517cfc7c3a0b28dc41
ongddree <ongddree@gmail.com>
2022-05-19 20:19:19 +0900

    ✨ viewchild decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnChanges, SimpleChange } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnChanges,
+  SimpleChange,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   commit = '';
   imgUrl = 'https://avatars.githubusercontent.com/u/93420227?v=4';
   count = 0;
   name!: string;
   private _customerName: string = '';
+  @ViewChild('nameRef') nameElementRef!: ElementRef;
+
+  ngAfterViewInit(): void {
+    this.nameElementRef.nativeElement.focus();
+    console.log(this.nameElementRef);
+  }
 
   get customerName(): string {
     return this._customerName;
