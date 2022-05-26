65c4d06a5c5756bb1356c8530eb7d4f1ec260451
ongddree <ongddree@gmail.com>
2022-05-27 08:19:19 +0900

    ✨ child 컴포넌트 생성

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ItemDetailComponent } from './item-detail/item-detail.component';
+import { ChildComponent } from './child/child.component';
 @NgModule({
-  declarations: [AppComponent, ItemDetailComponent],
+  declarations: [AppComponent, ItemDetailComponent, ChildComponent],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
